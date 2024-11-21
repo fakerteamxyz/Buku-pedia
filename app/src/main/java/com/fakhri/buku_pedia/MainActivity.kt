d7a2c04d3467ecad481044dea857fe17bf4ef150
Fakhri Aditia Rahman <fakhriaditiarahman12@gmail.com>
2024-11-22 00:57:27 +0700

    fix FE dan BE

--- app/src/main/java/com/fakhri/buku_pedia/MainActivity.kt
+++ app/src/main/java/com/fakhri/buku_pedia/MainActivity.kt
@@ -3,49 +3,32 @@ package com.fakhri.buku_pedia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.fakhri.buku_pedia.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        // Set default fragment (HomeFragment)
-        loadFragment(HomeFragment())
-
-        // Bottom Navigation item click listener
-        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_home -> {
-                    loadFragment(HomeFragment())
-                    true
-                }
-                R.id.nav_categories -> {
-                    loadFragment(CategoriesFragment())
-                    true
-                }
-                R.id.nav_cart -> {
-                    loadFragment(CartFragment())
-                    true
-                }
-                R.id.nav_account -> {
-                    loadFragment(AccountFragment())
-                    true
-                }
-                else -> false
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.nav_home -> selectedFragment = HomeFragment()
+                R.id.nav_categories -> selectedFragment = CategoriesFragment()
+                R.id.nav_cart -> selectedFragment = CartFragment()
+                R.id.nav_account -> selectedFragment = AccountFragment()
             }
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit()
+            }
+            true
         }
-    }
 
-    // Function to replace fragment
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment, fragment)
-            .commit()
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_home
     }
 }
